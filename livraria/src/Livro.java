7e410e94dd1b4bfeb5973c9675c793001ca01ab5
amilcarparada <amilcarparada@gmail.com>
2025-03-13 12:26:56 -0300

    Projeto livraria

---
+++ livraria/src/Livro.java
@@ -0,0 +1,12 @@
+public class Livro {
+// Atributo tipo nomeAtributo;
+    String titulo;
+    String autor;
+    double preço;
+    String editora;
+    int páginas;
+    String resumo;
+    String tipoCapa;
+
+
+}
